eef909944147bc83513f0471513af90f05bbfb8b
Omar <omar@barmej.com>
2020-06-27 01:12:50 +0300

    speedtest

---
+++ SpeedTest5/ViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func red() {
+        view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        
+    }
+    
+    @IBAction func blue() {
+        view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+    }
+    
+    
+    
+    @IBAction func yellow() {
+        view.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+    }
+    
+    @IBAction func green() {
+        view.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+    }
+    
+    
+}
+
