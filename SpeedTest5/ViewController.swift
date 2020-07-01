8d010831877ef1bc24216f77f8f4641cbf81362a
wroodalrashidi <66529914+wroodalrashidi@users.noreply.github.com>
2020-07-01 17:23:05 +0300

    done

--- SpeedTest5/ViewController.swift
+++ SpeedTest5/ViewController.swift
@@ -2,6 +2,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+   
+    
     @IBAction func red() {
         view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         
